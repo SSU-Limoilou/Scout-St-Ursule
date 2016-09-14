3f647bbf2a67a4a02a9f69bdfa3f02204d94a7a3
Admin <scout@scout.com>
2016-09-14 12:57:01 -0400

    Backed up the database with Revisr.

---
+++ wp-content/uploads/revisr-backups/revisr_wp_revisr.sql
@@ -0,0 +1,39 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `wp_revisr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wp_revisr` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `message` text,
+  `event` varchar(42) NOT NULL,
+  `user` varchar(60) DEFAULT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOCK TABLES `wp_revisr` WRITE;
+/*!40000 ALTER TABLE `wp_revisr` DISABLE KEYS */;
+INSERT INTO `wp_revisr` VALUES (1,'2016-09-09 15:26:59','Checked out branch: developpement.','branch','Admin'),(2,'2016-09-09 15:27:39','Pulled <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=137776f\">#137776f</a> from origin/developpement.','pull','Admin'),(3,'2016-09-09 15:38:00','Successfully backed up the database.','backup','Admin'),(4,'2016-09-09 15:38:03','Pulled <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=166e089\">#166e089</a> from origin/developpement.','pull','Admin'),(5,'2016-09-09 15:41:30','Error pushing changes to the remote repository.','error','Admin'),(6,'2016-09-09 15:42:16','Error pushing changes to the remote repository.','error','Admin'),(7,'2016-09-09 15:44:14','Created new branch: faq_scout','branch','Admin'),(8,'2016-09-09 15:44:15','Checked out branch: faq_scout.','branch','Admin'),(9,'2016-09-09 15:48:04','Successfully backed up the database.','backup','Admin'),(10,'2016-09-09 15:48:05','Committed <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=b105206&success=true\">#b105206</a> to the local repository.','commit','Admin'),(11,'2016-09-09 15:48:51','Successfully pushed 3 commits to origin/faq_scout.','push','Admin'),(12,'2016-09-09 18:59:38','Successfully backed up the database.','backup','Admin'),(13,'2016-09-09 18:59:40','Committed <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=4f457d7&success=true\">#4f457d7</a> to the local repository.','commit','Admin'),(14,'2016-09-09 19:31:50','There was an error committing the changes to the local repository.','error','Admin'),(15,'2016-09-09 19:32:22','There was an error committing the changes to the local repository.','error','Admin'),(16,'2016-09-09 19:32:46','Committed <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=0efa916&success=true\">#0efa916</a> to the local repository.','commit','Admin'),(17,'2016-09-09 20:54:10','Successfully backed up the database.','backup','Admin'),(18,'2016-09-09 20:54:11','Committed <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=5ccf147&success=true\">#5ccf147</a> to the local repository.','commit','Admin'),(19,'2016-09-09 20:55:24','Successfully pushed 3 commits to origin/faq_scout.','push','Admin'),(20,'2016-09-09 20:56:13','Created new branch: header_logo_scout','branch','Admin'),(21,'2016-09-09 20:56:13','Checked out branch: header_logo_scout.','branch','Admin'),(22,'2016-09-09 21:26:54','Successfully backed up the database.','backup','Admin'),(23,'2016-09-09 21:26:55','Committed <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=c9603c0&success=true\">#c9603c0</a> to the local repository.','commit','Admin'),(24,'2016-09-09 21:27:22','Successfully pushed 1 commit to origin/header_logo_scout.','push','Admin'),(25,'2016-09-13 14:51:54','Successfully backed up the database.','backup','Admin'),(26,'2016-09-13 14:53:05','Successfully backed up the database.','backup','Admin'),(27,'2016-09-13 14:53:09','Checked out branch: faq_scout.','branch','Admin'),(28,'2016-09-13 14:53:39','Successfully backed up the database.','backup','Admin'),(29,'2016-09-13 14:53:41','Checked out branch: faq_scout.','branch','Admin'),(30,'2016-09-13 14:54:41','Successfully backed up the database.','backup','Admin'),(31,'2016-09-13 14:54:43','Checked out branch: faq_scout.','branch','Admin'),(32,'2016-09-13 16:09:29','Successfully backed up the database.','backup','Admin'),(33,'2016-09-13 16:09:30','Committed <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=c174870&success=true\">#c174870</a> to the local repository.','commit','Admin'),(34,'2016-09-13 16:34:30','Successfully pushed 4 commits to origin/faq_scout.','push','Admin'),(35,'2016-09-14 18:25:06','Successfully backed up the database.','backup','Admin'),(36,'2016-09-14 18:25:07','Committed <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=8a6f3b5&success=true\">#8a6f3b5</a> to the local repository.','commit','Admin'),(37,'2016-09-14 18:25:18','Successfully pushed 3 commits to origin/faq_scout.','push','Admin'),(38,'2016-09-14 18:27:13','Successfully imported the database. ','import','Admin'),(39,'2016-09-14 18:28:10','Reverted to commit <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=4c5d164\">#4c5d164</a>.','revert','Admin'),(40,'2016-09-14 18:28:15','Successfully backed up the database.','backup','Admin'),(41,'2016-09-14 18:29:09','Reverted to commit <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=3b75aae\">#3b75aae</a>.','revert','Admin'),(42,'2016-09-14 18:29:13','Successfully backed up the database.','backup','Admin'),(43,'2016-09-14 18:30:19','Successfully backed up the database.','backup','Admin'),(44,'2016-09-14 18:30:20','Committed <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=ab9e95b&success=true\">#ab9e95b</a> to the local repository.','commit','Admin'),(45,'2016-09-14 18:30:30','Successfully pushed 8 commits to origin/developpement.','push','Admin'),(46,'2016-09-14 18:52:35','Successfully backed up the database.','backup','Admin'),(47,'2016-09-14 18:52:42','Committed <a href=\"http://localhost/wp-scout/wp-admin/admin.php?page=revisr_view_commit&commit=e05d3d4&success=true\">#e05d3d4</a> to the local repository.','commit','Admin'),(48,'2016-09-14 18:52:48','Successfully pushed 1 commit to origin/developpement.','push','Admin'),(49,'2016-09-14 18:53:34','Merged branch origin/video_scout into branch developpement.','merge','Admin'),(50,'2016-09-14 18:53:37','Successfully imported the database. ','import','Admin');
+/*!40000 ALTER TABLE `wp_revisr` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
---
+++ wp-content/uploads/revisr-backups/revisr_wp_usermeta.sql
@@ -0,0 +1,40 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `wp_usermeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wp_usermeta` (
+  `umeta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `meta_key` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `meta_value` longtext COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`umeta_id`),
+  KEY `user_id` (`user_id`),
+  KEY `meta_key` (`meta_key`(191))
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOCK TABLES `wp_usermeta` WRITE;
+/*!40000 ALTER TABLE `wp_usermeta` DISABLE KEYS */;
+INSERT INTO `wp_usermeta` VALUES (1,1,'nickname','Admin'),(2,1,'first_name',''),(3,1,'last_name',''),(4,1,'description',''),(5,1,'rich_editing','true'),(6,1,'comment_shortcuts','false'),(7,1,'admin_color','fresh'),(8,1,'use_ssl','0'),(9,1,'show_admin_bar_front','true'),(10,1,'wp_capabilities','a:1:{s:13:\"administrator\";b:1;}'),(11,1,'wp_user_level','10'),(12,1,'dismissed_wp_pointers',''),(13,1,'show_welcome_panel','1'),(14,1,'session_tokens','a:3:{s:64:\"e9a3ff459aad9bd3a28700dfba1183c892a03c5efc7160ad8eb9362a70756f55\";a:4:{s:10:\"expiration\";i:1473943790;s:2:\"ip\";s:3:\"::1\";s:2:\"ua\";s:109:\"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36\";s:5:\"login\";i:1473770990;}s:64:\"2297ccb738542a32dc6a04ec5a5b41f93a31f0606dff06c097feff820d57b425\";a:4:{s:10:\"expiration\";i:1474039635;s:2:\"ip\";s:3:\"::1\";s:2:\"ua\";s:109:\"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36\";s:5:\"login\";i:1473866835;}s:64:\"56a1b0feeeb29ee9cf412a86f96fc908ed349622178826d5222573073b82f0b7\";a:4:{s:10:\"expiration\";i:1474042813;s:2:\"ip\";s:3:\"::1\";s:2:\"ua\";s:109:\"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36\";s:5:\"login\";i:1473870013;}}'),(15,1,'wp_dashboard_quick_press_last_post_id','3'),(16,1,'manageedit-shop_ordercolumnshidden','a:1:{i:0;s:15:\"billing_address\";}'),(17,1,'wp_user-settings','editor=html&libraryContent=browse&imgsize=full&unfold=1&mfold=o'),(18,1,'wp_user-settings-time','1473866831'),(19,1,'managenav-menuscolumnshidden','a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}'),(20,1,'metaboxhidden_nav-menus','a:6:{i:0;s:30:\"woocommerce_endpoints_nav_link\";i:1;s:21:\"add-post-type-product\";i:2;s:12:\"add-post_tag\";i:3;s:15:\"add-post_format\";i:4;s:15:\"add-product_cat\";i:5;s:15:\"add-product_tag\";}'),(21,1,'nav_menu_recently_edited','10'),(22,1,'closedpostboxes_post','a:0:{}'),(23,1,'metaboxhidden_post','a:4:{i:0;s:11:\"postexcerpt\";i:1;s:13:\"trackbacksdiv\";i:2;s:10:\"postcustom\";i:3;s:11:\"commentsdiv\";}');
+/*!40000 ALTER TABLE `wp_usermeta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
